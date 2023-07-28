f059c4d98b07eba873208e0335a23e8354c1d7e8
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-07-28 20:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const line = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  width: 200,
+  height: 2,
+  angle: 0,
+  speed: 2
+};
+
+function drawLine() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  ctx.save();
+  ctx.translate(line.x, line.y);
+  ctx.rotate(line.angle * Math.PI / 180);
+  ctx.fillStyle = "black";
+  ctx.fillRect(-line.width / 2, -line.height / 2, line.width, line.height);
+  ctx.restore();
+  
+  line.angle += line.speed;
+  
+  requestAnimationFrame(drawLine);
+}
+
+drawLine();
